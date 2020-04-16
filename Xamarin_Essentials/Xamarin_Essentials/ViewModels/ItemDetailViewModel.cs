bce1568e1209c9ca70db0c99131ba8e3c6db6f30
rikupin1105 <41769991+rikupin1105@users.noreply.github.com>
2020-04-16 23:31:37 +0900

    テンプレートを作成

---
+++ Xamarin_Essentials/Xamarin_Essentials/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin_Essentials.Models;
+
+namespace Xamarin_Essentials.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
