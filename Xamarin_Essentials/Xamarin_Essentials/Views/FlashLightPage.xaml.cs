1400e2aeae67ce54fd6f0922c938aba4ebc09885
rikupin1105 <41769991+rikupin1105@users.noreply.github.com>
2020-04-16 23:56:59 +0900

    フラッシュライトを追加

---
+++ Xamarin_Essentials/Xamarin_Essentials/Views/FlashLightPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin_Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+
+namespace Xamarin_Essentials.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FlashLightPage : ContentPage
+    {
+        public FlashLightPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_On_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                await Flashlight.TurnOnAsync();
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private async void Button_Off_Clicked(ssobject sender, EventArgs e)
+        {
+            try
+            {
+                await Flashlight.TurnOffAsync();
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
