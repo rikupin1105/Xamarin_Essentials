ad5fb1fa2dfdfdf1010f7fef2b77ffafaab1b19f
rikupin1105 <41769991+rikupin1105@users.noreply.github.com>
2020-04-17 14:19:49 +0900

    コンパスを追加

---
+++ Xamarin_Essentials/Xamarin_Essentials/ViewModels/CompassViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Xamarin_Essentials.ViewModels
+{
+    class CompassViewModel : BaseViewModel
+    {
+        string headingDisplay;
+        double heading;
+        double rotationHeading;
+
+        
+        public CompassViewModel()
+        {
+            Title = "Compass";
+            Stop();
+            Start();
+        }
+        public string HeadingDisplay
+        {
+            get => headingDisplay;
+            set => SetProperty(ref headingDisplay, value);
+        }
+
+        public double Heading
+        {
+            get => heading;
+            set => SetProperty(ref heading, value);
+        }
+
+        public double RotationHeading
+        {
+            get => rotationHeading;
+            set => SetProperty(ref rotationHeading, value);
+        }
+
+        void Stop()
+        {
+            if (!Compass.IsMonitoring)
+                return;
+
+            Compass.ReadingChanged -= Compass_ReadingChanged;
+            Compass.Stop();
+        }
+        void Start()
+        {
+            if (Compass.IsMonitoring)
+                return;
+
+            Compass.ReadingChanged += Compass_ReadingChanged;
+            Compass.Start(SensorSpeed.UI);
+
+        }
+
+        void Compass_ReadingChanged(object sender, CompassChangedEventArgs e)
+        {
+            Heading = e.Reading.HeadingMagneticNorth;
+            HeadingDisplay = $"{Math.Round(Heading)}";
+            RotationHeading = 360 - Heading;
+        }
+    }
+
+}
